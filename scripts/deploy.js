429f777945f6dc3fe2d7ead0dc5bb6fc89bd980d
dannylincs <dannysocrate@gmail.com>
2023-07-11 17:05:16 +0100

    a

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
+  const chai = await Chai.deploy(); //creating an instance of our smart contract
+
+  await chai.deployed();//deploying your smart contract
+
+  console.log("Deployed contract address:",`${chai.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+//0x3A0160fF511fe0e3F626146f896f64a451f7C828
